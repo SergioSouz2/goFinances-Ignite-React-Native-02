90b17c5898138bac206eefd87905cbf85d929078
SergioSouz2 <sergio.tbl0123@gmail.com>
2023-10-08 18:08:50 -0300

    CREATE button TransactionType de (selecionar o tipo de transacao)

--- src/screens/Register/Register.tsx
+++ src/screens/Register/Register.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Input } from "../../components/Form/Input/Input";
 import { Button } from "../../components/Form/Button/Button";
 
-import { Container, Header, Title, Form, Fields } from "./styles";
+import {
+   Container,
+   Header,
+   Title,
+   Form,
+   Fields,
+   TransactionTypes,
+} from "./styles";
+import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton/TransactionTypeButton";
 
 export function Register() {
+   const [transactionType, setTransactionType] = useState("");
+
+   function handleTrasactionTypeSelect(type: "up" | "down") {
+      // TODO:
+      setTransactionType(type);
+   }
+
    return (
       <Container>
          <Header>
@@ -15,6 +30,21 @@ export function Register() {
             <Fields>
                <Input placeholder="Nome" />
                <Input placeholder="Preco" />
+
+               <TransactionTypes>
+                  <TransactionTypeButton
+                     type="up"
+                     title="Income"
+                     onPress={() => handleTrasactionTypeSelect("up")}
+                     isActive={transactionType === "up"}
+                  />
+                  <TransactionTypeButton
+                     type="down"
+                     title="Outcome"
+                     onPress={() => handleTrasactionTypeSelect("down")}
+                     isActive={transactionType === "down"}
+                  />
+               </TransactionTypes>
             </Fields>
 
             <Button title="Enviar" />
